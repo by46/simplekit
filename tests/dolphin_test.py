447a12fb78e6b83239ba169f125e3dd3fb3ee330
Benjamin Yan <ycs_ctbu_2010@126.com>
2015-11-19 22:30:19 +0800

    refactor dolphin

--- tests/dolphin_test.py
+++ tests/dolphin_test.py
@@ -8,7 +8,7 @@ except ImportError:
 import unittest
 import json
 
-from simplekit import dolphin
+from simplekit import objson
 
 
 class DolphinTestCase(unittest.TestCase):
@@ -20,12 +20,12 @@ class DolphinTestCase(unittest.TestCase):
                  ('false', False),
                  ('[]', [])]
         for text, expected in tests:
-            actual = dolphin.loads(text)
+            actual = objson.loads(text)
             self.assertEqual(expected, actual)
 
     def test_loads_normal(self):
         text = r'{"sort":true, "name":{"first":"benjamin", "last": "yan"}}'
-        obj = dolphin.loads(text)
+        obj = objson.loads(text)
         self.assertIsNotNone(obj)
         self.assertTrue(obj.sort)
         self.assertEqual("benjamin", obj.name.first)
@@ -34,11 +34,11 @@ class DolphinTestCase(unittest.TestCase):
     def test_loads_exceptions(self):
         tests = ["{"]
         for text in tests:
-            self.assertIsNone(dolphin.loads(text))
+            self.assertIsNone(objson.loads(text))
 
     def test_loads_exceptions2(self):
         text = r'{"class":true, "def":true, "case":true}'
-        obj = dolphin.loads(text)
+        obj = objson.loads(text)
         self.assertTrue(obj.mclass)
         self.assertTrue(obj.mdef)
         self.assertTrue(obj.case)
@@ -47,65 +47,65 @@ class DolphinTestCase(unittest.TestCase):
         self.assertTrue(obj['def'])
 
         text = r'{"class":true, "mclass":false}'
-        obj = dolphin.loads(text)
+        obj = objson.loads(text)
         self.assertFalse(obj.mclass)
         self.assertTrue(obj['class'])
 
     def test_dumps(self):
         text = r'{"sort":true, "name":{"first":"benjamin", "last": "yan"}}'
-        obj = dolphin.loads(text)
-        actual = dolphin.dumps(obj)
+        obj = objson.loads(text)
+        actual = objson.dumps(obj)
         self.assertDictEqual(json.loads(text), json.loads(actual))
 
     def test_loads_knapsack(self):
         text = r'{"name":"benjamin", "age":21}'
-        obj = dolphin.loads(text)
-        obj2 = dolphin.loads(text)
+        obj = objson.loads(text)
+        obj2 = objson.loads(text)
         self.assertEqual(type(obj), type(obj2))
 
         text = r'{"age":21,"name":"benjamin"}'
-        obj3 = dolphin.loads(text)
+        obj3 = objson.loads(text)
         self.assertEqual(type(obj), type(obj3))
 
         text = r'{"age":21,"name":"benjamin1"}'
-        obj4 = dolphin.loads(text)
+        obj4 = objson.loads(text)
         self.assertEqual(type(obj), type(obj4))
 
         text = r'{"name":"benjamin", "age":"21"}'
-        obj5 = dolphin.loads(text)
+        obj5 = objson.loads(text)
         self.assertNotEqual(type(obj), type(obj5))
 
     def test_load_normal(self):
         fp = StringIO(r'{"name":"benjamin", "age":21}')
-        obj = dolphin.load(fp)
+        obj = objson.load(fp)
         self.assertIsNotNone(obj)
         self.assertEqual("benjamin", obj.name)
         self.assertEqual(21, obj.age)
 
     def test_dump_normal(self):
         text = r'{"name":"benjamin", "age":21}'
-        obj = dolphin.loads(text)
+        obj = objson.loads(text)
         fp = StringIO()
-        dolphin.dump(obj, fp)
+        objson.dump(obj, fp)
         self.assertEqual(json.loads(text), json.loads(fp.getvalue()))
 
     def test_dump_change_value(self):
         text = r'{"name":"benjamin", "age":21}'
-        obj = dolphin.loads(text)
+        obj = objson.loads(text)
         obj.age = 30
         fp = StringIO()
-        dolphin.dump(obj, fp)
+        objson.dump(obj, fp)
         expected = json.loads(text)
         expected['age'] = 30
         self.assertEqual(expected, json.loads(fp.getvalue()))
 
     def test_dynamic_class(self):
-        team = dolphin.make_dynamic_class("Team", "name,age,members")
+        team = objson.make_dynamic_class("Team", "name,age,members")
         t = team({'name': 'neweggtech', 'age': 5, 'members': []})
         self.assertEqual('neweggtech', t.name)
         self.assertEqual(5, t.age)
 
-        team = dolphin.make_dynamic_class("Team", "name,age,age, members")
+        team = objson.make_dynamic_class("Team", "name,age,age, members")
         t = team({'name': 'neweggtech', 'age': 5, 'members': []})
         self.assertEqual('neweggtech', t.name)
         self.assertEqual(5, t.age)
