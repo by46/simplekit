73432b7cd3b72266b5939a77a38a66cd871826f7
Benjamin Yan <ycs_ctbu_2010@126.com>
2015-11-22 19:48:32 +0800

    format

--- simplekit/objson/dynamic_class.py
+++ simplekit/objson/dynamic_class.py
@@ -1,6 +1,6 @@
-__author__ = 'benjamin.c.yan@newegg.com'
-
 from keyword import iskeyword as _iskeyword
+
+__author__ = 'benjamin.c.yan@newegg.com'
 
 
 def _item_setter(key):
@@ -17,12 +17,16 @@ def _item_getter(key):
     return _getter
 
 
-def __dynamic__init__(self, kv):
+def _dynamic__init(self, kv):
     self._data.update(kv)
 
 
-def __dynamic__setitem__(self, key, value):
+def _dynamic__setitem(self, key, value):
     self._data[key] = value
+
+
+def _property(name):
+    return property(_item_getter(name), _item_setter(name))
 
 
 def make_dynamic_class(typename, field_names):
@@ -40,7 +44,7 @@ def make_dynamic_class(typename, field_names):
     >>> __identifier__ = "dolphin"
     >>>
     >>> __slots__ = ('name', 'age')
-    >>> 
+    >>>
     >>> _data = dict()
     >>>
     >>> def __init__(self, kv):
@@ -64,8 +68,8 @@ def make_dynamic_class(typename, field_names):
     >>> age = property(_item_getter('age'), _item_setter('age'))
 
     :param typename: dynamic class's name
-    :param field_names: a string :func:`list` and a field name string which separated by comma,
-    ``['name', 'sex']`` or ``"name,sex"``
+    :param field_names: a string :class:`list` and a field name string which separated by comma,
+        ``['name', 'sex']`` or ``"name,sex"``
 
     :return: a class type
     """
@@ -75,20 +79,18 @@ def make_dynamic_class(typename, field_names):
 
     safe_fields_names = map(lambda x: 'm' + x if _iskeyword(x) else x, field_names)
 
-    make_property = lambda x: property(_item_getter(x), _item_setter(x))
-
-    attr = dict((safe_name, make_property(name)) for name, safe_name in zip(field_names, safe_fields_names))
+    attr = dict((safe_name, _property(name)) for name, safe_name in zip(field_names, safe_fields_names))
     attr['__doc__'] = typename
     attr['__identifier__'] = "dolphin"
     attr['__slots__'] = tuple(safe_fields_names)
     attr['_data'] = dict()
-    attr['__init__'] = __dynamic__init__
+    attr['__init__'] = _dynamic__init
     attr['__getitem__'] = lambda self, key: self._data.get(key)
-    attr['__setitem__'] = __dynamic__setitem__
+    attr['__setitem__'] = _dynamic__setitem
     attr['__iter__'] = lambda self: iter(self._data)
     attr['__repr__'] = lambda self: "{%s}" % (', '.join([
-        "%s=%r" % (key, self[key]) for key in sorted(self._data.keys())
-    ]))
+                                                            "%s=%r" % (key, self[key]) for key in
+                                                            sorted(self._data.keys())
+                                                            ]))
 
     return type(typename, (object,), attr)
-
