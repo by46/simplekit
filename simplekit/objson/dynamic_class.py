05681686fc1eff25d5b54565ff603fc3197d472c
Benjamin Yan <ycs_ctbu_2010@126.com>
2015-11-22 19:24:43 +0800

    format

--- simplekit/objson/dynamic_class.py
+++ simplekit/objson/dynamic_class.py
@@ -26,6 +26,49 @@ def __dynamic__setitem__(self, key, value):
 
 
 def make_dynamic_class(typename, field_names):
+    """a factory function to create type dynamically
+
+    The factory function is used by :func:`objson.load` and :func:`objson.loads`.
+    Creating the object deserialize from json string. The inspiration come from
+    :func:`collections.namedtuple`. the difference is that I don't your the class
+    template to define a dynamic class, instead of, I use the :func:`type` factory
+    function.
+
+    Class prototype definition:
+
+    >>> class JsonObject(object):
+    >>> __identifier__ = "dolphin"
+    >>>
+    >>> __slots__ = ('name', 'age')
+    >>> 
+    >>> _data = dict()
+    >>>
+    >>> def __init__(self, kv):
+    >>>     self._data.update(kv)
+    >>>
+    >>> def __getitem__(self, key):
+    >>>     return self._data.get(key)
+    >>>
+    >>> def __setitem__(self, key, value):
+    >>>     self._data[key] = value
+    >>>
+    >>> def __iter__(self):
+    >>>     return self._data.__iter__()
+    >>>
+    >>> def __repr__(self):
+    >>>    keys = sorted(self._data.keys())
+    >>>    text = ', '.join(["%s=%r" % (key, self[key]) for key in keys])
+    >>>    return '{%s}' % text
+    >>>
+    >>> name = property(_item_getter('name'), _item_setter('name'))
+    >>> age = property(_item_getter('age'), _item_setter('age'))
+
+    :param typename: dynamic class's name
+    :param field_names: a string :func:`list` and a field name string which separated by comma,
+    ``['name', 'sex']`` or ``"name,sex"``
+
+    :return: a class type
+    """
     if isinstance(field_names, basestring):
         field_names = field_names.replace(",", " ").split()
     field_names = map(str, field_names)
