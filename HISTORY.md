08222aab958788d032c51fe7c5d2cc662c567177
benjamin.c.yan <ycs_ctbu_2010@126.com>
2016-02-03 23:44:31 +0800

    new objson

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,7 @@
 History
 ======
+0.0.6
+
 0.0.5
 --------------
 1. refactor objson module
--- tests/objson_test.py
+++ tests/objson_test.py
@@ -102,7 +102,6 @@ class DolphinTestCase(unittest.TestCase):
         expected = objson.dumps([dict(name='benjamin', age=21)])
         self.assertEqual(json.loads(text), json.loads(expected))
 
-
     def test_dump_change_value(self):
         text = r'{"name":"benjamin", "age":21}'
         obj = objson.loads(text)
@@ -123,3 +122,87 @@ class DolphinTestCase(unittest.TestCase):
         t = team({'name': 'neweggtech', 'age': 5, 'members': []})
         self.assertEqual('neweggtech', t.name)
         self.assertEqual(5, t.age)
+
+
+class Dolphin2TestCase(unittest.TestCase):
+    def test_loads_basic(self):
+        tests = [(r'"ben"', "ben"),
+                 ('1', 1),
+                 ('3.1415', 3.1415),
+                 ('true', True),
+                 ('false', False),
+                 ('[]', [])]
+        for text, expected in tests:
+            actual = objson.loads2(text)
+            self.assertEqual(expected, actual)
+
+    def test_loads_normal(self):
+        text = r'{"sort":true, "name":{"first":"benjamin", "last": "yan"}}'
+        obj = objson.loads2(text)
+        self.assertIsNotNone(obj)
+        self.assertTrue(obj.sort)
+        self.assertEqual("benjamin", obj.name.first)
+        self.assertEqual("yan", obj.name.last)
+
+    def test_loads_exceptions(self):
+        tests = ["{"]
+        for text in tests:
+            self.assertIsNone(objson.loads(text))
+
+    def test_loads_exceptions2(self):
+        text = r'{"class":true, "def":true, "case":true}'
+        obj = objson.loads2(text)
+        self.assertTrue(obj.mclass)
+        self.assertTrue(obj.mdef)
+        self.assertTrue(obj.case)
+
+        self.assertTrue(obj['class'])
+        self.assertTrue(obj['def'])
+
+        text = r'{"class":true, "mclass":false, "from-cookie": true, "0file":true}'
+        obj = objson.loads2(text)
+        self.assertFalse(obj.mclass)
+        self.assertTrue(obj['class'])
+        self.assertTrue(obj.from_cookie)
+        self.assertTrue(obj['from-cookie'])
+        self.assertTrue(obj.m0file)
+        self.assertTrue(obj['0file'])
+
+    def test_dumps(self):
+        text = r'{"sort":true, "name":{"first":"benjamin", "last": "yan"}}'
+        obj = objson.loads2(text)
+        actual = objson.dumps(obj)
+        self.assertDictEqual(json.loads(text), json.loads(actual))
+
+    def test_load_normal(self):
+        fp = StringIO(r'{"name":"benjamin", "age":21}')
+        obj = objson.load2(fp)
+        self.assertIsNotNone(obj)
+        self.assertEqual("benjamin", obj.name)
+        self.assertEqual(21, obj.age)
+
+    def test_dump_normal(self):
+        text = r'{"name":"benjamin", "age":21}'
+        obj = objson.loads2(text)
+        fp = StringIO()
+        objson.dump(obj, fp)
+        self.assertEqual(json.loads(text), json.loads(fp.getvalue()))
+
+    def test_dump_normal2(self):
+        text = r'[{"name":"benjamin", "age":21}]'
+        obj = objson.loads2(text)
+        expected = objson.dumps(obj)
+        self.assertEqual(json.loads(text), json.loads(expected))
+
+        expected = objson.dumps([dict(name='benjamin', age=21)])
+        self.assertEqual(json.loads(text), json.loads(expected))
+
+    def test_dump_change_value(self):
+        text = r'{"name":"benjamin", "age":21}'
+        obj = objson.loads2(text)
+        obj.age = 30
+        fp = StringIO()
+        objson.dump(obj, fp)
+        expected = json.loads(text)
+        expected['age'] = 30
+        self.assertEqual(expected, json.loads(fp.getvalue()))
