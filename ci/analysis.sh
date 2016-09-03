e2139dd83da0d19dc031f898789dbce6041b9640
by46 <ycs_ctbu_2010@126.com>
2016-09-03 13:19:45 +0800

    add coverage

--- ci/analysis.sh
+++ ci/analysis.sh
@@ -11,6 +11,9 @@ chmod +x ./venv/bin/activate
 
 ./venv/bin/activate
 pip install --trusted-host scmesos06 -i http://scmesos06/simple -r requirements_dev.txt --cache-dir=/tmp/${JOB_NAME}
+[ $? -gt 0 ] && exit 1
+
 pip install --trusted-host scmesos06 -i http://scmesos06/simple Coverage --cache-dir=/tmp/${JOB_NAME}
+[ $? -gt 0 ] && exit 1
 
 ${PYLINT} -f parseable simplekit | tee pylint.out
