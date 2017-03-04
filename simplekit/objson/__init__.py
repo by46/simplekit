725719b8c52844b1618a3d38ba91fe1f25cd3c34
by46 <ycs_ctbu_2010@126.com>
2017-03-04 16:16:34 +0800

    feat: remove unuse code

--- simplekit/objson/__init__.py
+++ simplekit/objson/__init__.py
@@ -3,8 +3,8 @@ Used to deserialize and serialize json
 """
 
 # from .dolphin import load, loads, dump, dumps
-from .dolphin import make_dynamic_class
 from .dolphin2 import loads, load, dump, dumps, empty
+from .dynamic_class import make_dynamic_class
 
 __author__ = 'benjamin.c.yan'
 __all__ = ["load", "loads", "dump", "dumps", "make_dynamic_class"]
